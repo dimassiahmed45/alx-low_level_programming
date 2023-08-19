c64465cfc1d9fc0f1dbc2f147acdf567bda79396
ahmed1dimassi <ahmed1dimassi@gmail.com>
2023-08-19 10:25:24 +0100

    0x11

--- 0x11-printf/_printf.c
+++ 0x11-printf/_printf.c
@@ -1,72 +1,60 @@
-#include "main.h"
-#include <stdlib.h>
-#include <ctype.h>
+#include <stdarg.h>
+#include <unistd.h>
 
-/**
- *
- * @c: the character to check
- * Return: 1 if the character is alphabetic, 0 otherwise
- */
-int is_alpha(char c)
-{
-	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+typedef struct {
+    char specifier;
+    void (*function)(va_list *, int *);
+} format_specifier;
+
+void print_char(va_list *args, int *count) {
+    char c = va_arg(*args, int);
+    write(1, &c, 1);
+    (*count)++;
+}
+
+void print_string(va_list *args, int *count) {
+    char *str = va_arg(*args, char *);
+    while (*str) {
+        write(1, str, 1);
+        str++;
+        (*count)++;
+    }
+}
+
+void print_percent(va_list *args, int *count) {
+    write(1, "%", 1);
+    (*count)++;
 }
 
 static format_specifier format_specifiers[] = {
-	{"c", print_char},
-	{"s", print_string},
-	{"%", print_percent},
-	{"d", print_integer},
-	{"i", print_integer},
-	{"b", print_binary},
-	{"o", print_octal},
-	{"x", print_hex},
-	{"X", print_hex},
-	{"u", print_unsigned},
-	{"p", print_address},
-	{"r", print_reverse},
-	{"R", print_rot13},
-	{"S", print_string_non_printable},
-	{NULL, NULL}
+    {'c', print_char},
+    {'s', print_string},
+    {'%', print_percent},
+    {0, NULL}
 };
 
-/**
- * _printf - produces output according to a format
- * @format: character string
- * Return: number of characters printed
- */
-int _printf(const char *format, ...)
-{
-	int count = 0, i;
-	format_t f = {0, -1, -1, -1, -1};
-	va_list args;
+int _printf(const char *format, ...) {
+    int count = 0;
+    va_list args;
+    va_start(args, format);
 
-	/* Note: printf segfaults if format is NULL */
-	if (!format)
-		return (-1);
+    while (*format) {
+        if (*format == '%') {
+            format++;
+            for (int i = 0; format_specifiers[i].specifier; ++i) {
+                if (*format == format_specifiers[i].specifier) {
+                    format_specifiers[i].function(&args, &count);
+                    break;
+                }
+            }
+            format++;
+        } else {
+            write(1, format, 1);
+            format++;
+            count++;
+        }
+    }
 
-	va_start(args, format);
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++;
-			f = get_format(&format);
-			if (f.flags == NULL)
-				return (-1);
-
-			for (i = 0; format_specifiers[i].specifier; ++i)
-				if (f.specifier == *format_specifiers[i].specifier)
-				{
-					format_specifiers[i].function(args, f, &count);
-					format++;
-					break;
-				}
-			free(f.flags);
-		}
-		else
-			_putchar(*format++, &count);
-	}
-	va_end(args);
-	return (count);
+    va_end(args);
+    return count;
 }
